5cdc4a11ec021c93656c6b5ea2d832f225381f3f
jasimkareem <147159170+Jasimkareem04@users.noreply.github.com>
2024-09-30 21:48:51 +0530

    Snake growing and GameOver

--- Snake2d/Assets/Scripts/Snake.cs
+++ Snake2d/Assets/Scripts/Snake.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 public class SnakeMovement : MonoBehaviour
 {
     private Vector2 dir = Vector2.right;
-   
+
+    private List<Transform> _segments = new List<Transform>();
+
+    public Transform Tail;
+
+    private void Start()
+    {
+        _segments = new List<Transform>();
+        _segments.Add(this.transform);
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -28,9 +38,46 @@ public class SnakeMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        for(int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[ i - 1 ].position;
+        }
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + dir.x,
             Mathf.Round(this.transform.position.y) + dir.y,
             0);
+    }
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.Tail);
+        segment.position = _segments[_segments.Count - 1].position;
+        
+        _segments.Add(segment);
+    }
+
+    private void ResetState()
+    {
+        for(int i=1; i< _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+        
+        _segments.Clear();
+        _segments.Add(this.transform);
+
+        this.transform.position = Vector3.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            Grow();
+        }
+        else if (collision.tag == "Obstacle")
+        {
+            ResetState();
+        }
     }
 }
